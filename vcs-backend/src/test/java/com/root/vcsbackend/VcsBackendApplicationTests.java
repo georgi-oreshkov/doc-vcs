c0e55beb9fe6c97526ac3c31d43f0ebbb766e8ba
Kr1s05 <kristianoreshkov04@gmail.com>
2026-03-10 20:25:27 +0200

    frontend and backend projects

---
+++ vcs-backend/src/test/java/com/root/vcsbackend/VcsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root.vcsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
